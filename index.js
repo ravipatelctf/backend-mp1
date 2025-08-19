70adf9da571ae34a1444aa9f11aa20f5ae84a82a
ravipatelctf <ravipatelctf@gmail.com>
2025-08-19 15:15:53 +0530

    added user model and routes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const {initializeDatabase} = require("./db/db.connect");
 const Product = require("./models/product.models");
-
+const User = require("./models/user.models");
 
 
 const express = require("express");
@@ -52,6 +52,122 @@ initializeDatabase();
 // seedData();
 // --------------------------------------------------------------------------
 
+// --------------------------------------------------------------------------
+
+// async function seedUsers() {
+//     try {
+//         const newUser = new User({
+//             name: "Test User 1",
+//             emailId: "test@user1.example",
+//             phoneNumber: 1234567890,
+//             address: ["Test address 1", "Test address 2", "Test address 3"],
+//         });
+//         await newUser.save();
+//         console.log("User DB seeded with a test user successfully.")
+//     } catch (error) {
+//         throw error;
+//     }
+// }
+// seedUsers()
+// --------------------------------------------------------------------------
+// user routes
+// --------------------------------------------------------------------------
+// get user by id
+async function readUserById() {
+    try {
+        const user = await User.findById("68a35f768fc5c3d122846f6b").populate("orders");
+        return user;
+    } catch (error) {
+        throw error;
+    }
+}
+
+app.get("/api/user", async (req, res) => {
+    try {
+        const user = await readUserById();
+        if (user) {
+            res
+                .status(200)
+                .send(user)
+        } else {
+            res
+                .status(404)
+                .json({error: "user not found!"});
+        }
+    } catch (error) {
+        res
+            .status(500)
+            .json({error: "Failed to fetch user!"})
+    }
+})
+// --------------------------------------------------------------------------
+// find usera and populate it's orders field with products ordered
+
+async function readUserAndPopulateOrders(productId) {
+    try {
+        const user = await User.findOne();
+        if (!user) {
+            throw new Error("User Not Found!")
+        }
+
+        user.orders.push(productId);
+        const updatedUser = await user.save();
+
+        return await updatedUser.populate("orders");
+    } catch (error) {
+        throw error;
+    }
+}
+
+app.post("/api/user/orders", async (req, res) => {
+    try {
+        const {productId} = req.body;
+        const updatedUser = await readUserAndPopulateOrders(productId);
+        res
+            .status(200)
+            .send(updatedUser)
+    } catch (error) {
+        res
+            .status(500)
+            .json({error: "Failed to update data"})
+    }
+})
+
+// --------------------------------------------------------------------------
+// update user address by id
+async function readUserByIdAndUpdateAddress(dataToUpdate) {
+    try {
+        const {address} = dataToUpdate;
+        const updatedUser = await User.findByIdAndUpdate("68a35f768fc5c3d122846f6b", {address}, {new: true}).populate("orders");
+        return updatedUser;
+    } catch (error) {
+        throw error;
+    }
+}
+
+app.post("/api/user", async (req, res) => {
+    try {
+        const updatedUser = await readUserByIdAndUpdateAddress(req.body);
+        if (updatedUser) {
+            res
+                .status(200)
+                .send(updatedUser)
+        } else {
+            res
+                .status(404)
+                .json({error: "user not found"})
+        }
+        
+    } catch (error) {
+        res
+            .status(500)
+            .json({error: "Failed to update data"})
+    }
+})
+
+// --------------------------------------------------------------------------
+// products routes
+// --------------------------------------------------------------------------
 async function readAll() {
     try {
         const allProducts = await Product.find();
---
+++ models/user.models.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    emailId: {
+        type: String,
+        required: true,
+    },
+    phoneNumber: {
+        type: Number,
+        required: true,
+    },
+    address: [{
+        type: String,
+        required: true,
+    }],
+    orders: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product"
+    }]
+},
+{
+    timestamps: true
+},
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
